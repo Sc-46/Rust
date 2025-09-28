a85d6c10c812b6a28a1e3078cd0038d2e3806a53
Sc46 <sc46@qq.com>
2025-09-28 19:16:37 +0800

    v0.6

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ mod tools;
 use crate::tools::*;
 
 fn main() {
-    println!("大风起兮云飞扬");
     let x=14;
     println!("x={}",x);
     println!("x^2={}",square(x));
     println!("x^3={}",cube(x));
-    println!("{}",multiply(327,950));
 }
--- src/tools.rs
+++ src/tools.rs
@@ -5,7 +5,3 @@ pub fn square(x:i32) -> i32 {
 pub fn cube(x:i32) -> i32 {
     x*square(x)
 }
-
-pub fn multiply(x:i32,y:i32) -> i32 {
-    x*y
-}
